fe14b5c53285a1ace5ebbd2a6db36e3f0eb47df6
flavianna <flaviannabe@gmail.com>
2023-01-10 21:18:53 -0300

    feat: Implementando tabela com os gastos (requisito 06).

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
     );
   }
 }
-
-export default Table;
+export default connect()(Table);
